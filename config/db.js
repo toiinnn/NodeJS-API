0d52217b701472b7c93719befa0557e775231fce
Antonio Marcos Oliveira <marcos_antonio97@live.com>
2019-10-07 16:55:04 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+// Módulo de configuração
 module.exports = {
     url: "mongodb://<user_name>:<password>@cluster0-shard-00-00-qjzc5.mongodb.net:27017,cluster0-shard-00-01-qjzc5.mongodb.net:27017,cluster0-shard-00-02-qjzc5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
